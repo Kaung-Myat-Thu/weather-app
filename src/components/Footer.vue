5203c264e2cf3cff292016c10d85ab8470e38cf1
Kaung Myat Thu <kaungmyatthu111319@gmail.com>
2022-08-26 12:47:17 +0630

    add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="">
+    <h3
+      class="text-center text-2xl absolute bottom-8 left-0 right-0 text-gray-800 font-bold font-mono"
+    >
+      Copy right&copy; 2022
+    </h3>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
